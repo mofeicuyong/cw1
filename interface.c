7b9e5344a98efb2ed6913192cc73ea4d26c79a9e
mofeicuyong <2919886718@qq.com>
2021-03-18 16:08:21 +0800

    an interface

---
+++ interface.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void menu1()
+{
+	printf("Please choose an option:\n");
+	printf("1)Register an account\n");
+	printf("2)Login\n");
+	printf("3)Search for books\n");
+	printf("4)Display all books\n");
+	printf("5)Quit\n");
+	printf(" Option:");
+}
+void menu2()
+{
+	printf("Please choose an option:\n");
+	printf("1)Add a book\n");
+	printf("2)Remove a book\n");
+	printf("3)Search for books\n");
+	printf("4)Display all books\n");
+	printf("5)Log out\n");
+	printf(" Option:");
+}
+void menu3()
+{
+	printf("Please choose an option:\n");
+	printf("1)Find books by title\n");
+	printf("2)Find books by author\n");
+	printf("3)Find books by years\n");
+	printf("4)Return to previous menu\n");
+	printf(" Option:");
+}
+void menu4()
+{
+	printf("Please choose an option:\n");
+	printf("1)Borrow a book\n");
+	printf("2)Return a book\n");
+	printf("3)Search for books\n");
+	printf("4)Display all books\n");
+	printf("5)Log out");
+	printf(" Option:");
+}
