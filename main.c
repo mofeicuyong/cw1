fec3ab7b0490632d12fcc803a8cdc6e1c3485b7b
mofeicuyong <2919886718@qq.com>
2021-03-26 15:52:44 +0800

    almost

--- main.c
+++ main.c
@@ -1,74 +1,135 @@
-#include "book_management .c"
-#include "interface.c"
+#include "book_management.c"
+#include "student.c"
 int main()
 {
-	head = tail = (Book*)malloc(sizeof(Book));
-	h=t=(struct BookArray*)malloc(sizeof(BookArray));
+	Book book;
+	printf("Welcome to use this system:\n");
+	Student* hh,*tt;
+	hh=tt=(Student*)malloc(sizeof(Student));
+	hh->next=NULL;
+	Book* head,*tail;
+	head=(Book*)malloc(sizeof(Book));
+	head->next=NULL;
+	Book* h,*t;
+	h=(Book*)malloc(sizeof(Book));
+	h->next=NULL;
 	int a;
-	char au;
-	char ti;
-	int y;
+	int time=0;
+	int kk;
+	BookArray x;
 	while(1)
-	{
+	{ 
 		menu1();
-		scanf("%d",a);
+		scanf("%d",&a);
 		switch(a)
 		{
 			case 1:
-			
+				register_id(hh);
+				continue;
 			case 2:
-			
-			case 3: 
-				menu3();
-				scanf("%d",a);
-				switch(a)
+				kk=log_in(hh);
+				if(kk==1)
 				{
-					case 1:
-						printf("Please enter the title:");
-						scanf("%s",ti);
-						BookArray find_book_by_title (ti);
-						BookArray *k=h;
-						while(k->next!=NULL)
-							{
-							printf("%d %s %s %d %d",k->array->id,k->array->title,k->array->authors,k->array->year,k->array->copies);
-							k=k->next;
-							}
-						continue;
-					case 2:
-						printf("Please enter the author:");
-						scanf("%s",au);
-						BookArray find_book_by_author (au);
-						BookArray *m=h;
-						while(m->next!=NULL)
-							{
-							printf("%d %s %s %d %d",m->array->id,m->array->title,m->array->authors,m->array->year,m->array->copies);
-							m=m->next;
-							}
-						continue;
-					case 3:
-						printf("Please enter the year:");
-						scanf("%d",y);
-						BookArray find_book_by_year (y);
-						BookArray *n=h;
-						while(n->next!=NULL)
-							{
-							printf("%d %s %s %d %d",n->array->id,n->array->title,n->array->authors,n->array->year,n->array->copies);
-							n=n->next;
-							}
-						continue;
-					case 4:
-						break;
+					time=0;
+					while(time==0)
+					{
+						menu2();
+						scanf("%d",&a);
+						switch(a)
+						{
+							case 1:
+								add_book(head);
+								continue;
+							case 2:
+								remove_book(head);
+								continue;
+							case 3:
+								
+								search(head,h);
+								continue;
+							case 4:
+								{
+								Book *p=head;
+								while(p->next!=NULL)
+								{
+									p=p->next;
+									printf("%d %s %s %d %d",p->id,p->title,p->authors,p->year,p->copies);
+									printf("\n");
+								}
+								continue;
+								}
+							case 5:
+								time=1;
+								break;
+							default:
+      							printf("Illegal input!\n");
+      							continue;
+							
+						}
+					}
 				}
+				else if(kk==0)
+				{
+					time=0;
+					while(time==0)
+					{
+					menu4();
+					scanf("%d",&a);
+					switch(a)
+					{
+						case 1:
+							Borrow_book(head);
+							continue;
+						case 2:
+							return_book(head);
+							continue;
+						case 3:
+							search(head,h);
+							continue;
+						case 4:
+								{
+					Book *p=head;
+					while(p->next!=NULL)
+					{
+						p=p->next;
+						printf("%d %s %s %d %d",p->id,p->title,p->authors,p->year,p->copies);
+						printf("\n");
+					}
+					continue;
+				}
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						case 5:
+							time=1;
+							continue;
+						default:
+      						printf("Illegal input!\n");
+      						continue;
+							
+					}
+					}
+				}
+				continue;
+			case 3: 
+				search(head,h);
 				continue;
 			case 4:
 				{
-				Book *p=head;
-				while(p->next!=NULL);
-				{
-					printf("%d %s %s %d %d",p->id,p->title,p->authors,p->year,p->copies);
-					p=p->next;
-				}
-				continue;
+					Book *p=head;
+					while(p->next!=NULL)
+					{
+						p=p->next;
+						printf("%d %s %s %d %d",p->id,p->title,p->authors,p->year,p->copies);
+						printf("\n");
+					}
+					continue;
 				}
 			case 5: 
 			return 0;
