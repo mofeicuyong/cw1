95089db3175f4dc45b566da7e5eb404a5a9eb09f
mofeicuyong <2919886718@qq.com>
2021-03-28 14:11:59 +0800

    change

--- student.c
+++ student.c
@@ -10,17 +10,19 @@ int register_id(Student *hh)
 	int flag=0;
 	Student *d;
 	d=hh;
+	int j=0;
 	i = (Student*)malloc(sizeof(Student));
 	i->id = (char*)malloc(sizeof(char)*50);
 	i->password = (char*)malloc(sizeof(char)*50);
 	printf("Please enter your user name:");
-	scanf("%s",i->id);
+	getchar();
+	scanf("%[^\n]s",i->id);
 	getchar();
 	while(strcmp(i->id,"librarian")==0)
 		{
 			printf("You can not register this name.Please type another one.\n");
 			printf("Please enter your user name:");
-			scanf("%s",i->id);
+			scanf("%[^\n]s",i->id);
 			getchar();
 		}
 	while(d->next!=NULL) 
@@ -31,18 +33,20 @@ int register_id(Student *hh)
 		printf("This user name has been used.Please change another one.\n");
 		flag=1;
 		}
+		j++;
 	}
 	d=hh;
 	if(flag==0)
 	{
 		printf("Please enter your password:");
-		scanf("%s",i->password);
+		scanf("%[^\n]s",i->password);
 		getchar();
 		while(d->next!= NULL)
 		{
         d = d->next;
     	}
       	d->next = i;
+      	i->idi=j;
       	printf("Register successfully!\n");
 		return 1;
 	}
@@ -53,18 +57,20 @@ int log_in(Student *hh)
 	Student *k;
 	i = (Student*)malloc(sizeof(Student));
 	k=hh;
+	int ab=0;
 	int flag=0;
 	i->id = (char*)malloc(sizeof(char)*50);
 	i->password = (char*)malloc(sizeof(char)*50);
 	printf("Please enter your user name:");
-	scanf("%s",i->id);
+	getchar();
+	scanf("%[^\n]s",i->id);
 	getchar();
 	printf("Please enter your password:");
-	scanf("%s",i->password);
+	scanf("%[^\n]s",i->password);
 	getchar();
 	if(strcmp(i->id,"librarian")==0&&strcmp(i->password,"librarian")==0)
 	{
-		return 1;
+		return -2;
 	}
 	else
 	{
@@ -75,7 +81,8 @@ int log_in(Student *hh)
 			if(strcmp(k->id,i->id)==0&&strcmp(k->password,i->password)==0)
 			{
 				flag=1;
-				return 0;
+				ab=k->idi;
+				return ab;
 			}
 		}
 		if(flag==0)
@@ -86,9 +93,15 @@ int log_in(Student *hh)
 	}
 	
 }
-int Borrow_book(Book *head)
+int Borrow_book(Book *head,int sid,Student*hh,Book*hhh)
 {
+	Student*k;
 	Book* m=head;
+	Book* w=hhh;
+	Book *node;
+	node = (Book*)malloc(sizeof(Book));
+	node->title =(char*)malloc(sizeof(char)*50);
+	node->authors =(char*)malloc(sizeof(char)*50);
 	int di;
 	int flag=0;
 	printf("Please enter the id of the book you want to borrow:");
@@ -101,43 +114,107 @@ int Borrow_book(Book *head)
 			flag=1;
 			if(m->copies>0)
 			{
-			m->copies--;
-			printf("you borrow this book successfully!");
+			k=hh;
+			while(k->next!=NULL)
+			{
+				k=k->next;
+				if(k->idi==sid)
+				{
+					k->book++;
+					if(k->book==1)
+					{
+					k->bro=m;
+					}
+					while(w->next!=NULL)
+					{
+						w=w->next;
+					}
+					if(w->id==di)
+					{
+						printf("You have already borrowed this book.\n");
+						return 0;
+					}
+					node->title=m->title;
+					node->authors=m->authors;
+					node->id=m->id;
+					node->year=m->year;
+					node->copies=1;
+					w->next=node;
+					m->copies--;
+				}
+			}
 			
+			printf("you borrow this book successfully!\n");
 			}
 			else
 			{
-				printf("This book has already been check out!");
+				printf("This book has no more left!");
 				return 0;
 			}
 		}
 	}
 	if(flag==0)
 	{
-		printf("Fail to find this book.");
+		printf("Fail to find this book.\n");
 		return 0;
 	}
 }
-int return_book(Book *head)
+int return_book(Book *head,int sid,Student* hh,Book*hhh)
 {
 	Book* m=head;
 	int di;
+	Book *we=hhh;
 	int flag=0;
+	Student*k=hh;
+	int flag2=0;
+	while(k->next!=NULL)
+			{
+				k=k->next;
+				if(k->idi==sid)
+				{
+								while(we->next!=NULL)
+								{
+									flag2=1;
+									we=we->next;
+									printf("%d %s %s %d %d",we->id,we->title,we->authors,we->year,we->copies);
+									printf("\n");
+								}
+						
+				}
+			}
+	if(flag2==0)
+	{
+		printf("You have no book need to return.\n");
+		return 0;
+	}
 	printf("Please enter the id of the book you want to return:");
 	scanf("%d",&di);
+	we=hhh;
 	while(m->next!=NULL)
 	{
 		m=m->next;
 		if(di==m->id)
 		{
-			flag=1;
 			m->copies++;
-			printf("You have return the book successfuly!");
+			flag=1;
+			while(we->next!=NULL)
+			{
+				if(we->next->id==di)
+				{
+						Book* pr=we->next; 
+						we->next=pr->next;
+						free(pr);
+					
+				}
+				if(we->next!=NULL)
+				we=we->next;
+			}
+			printf("You have return the book successfuly!\n");
 		}
 	}
 	if(flag==0)
 	{
-		printf("You may type a wrong id.");
+		printf("You may type a wrong id.\n");
 	}
 	
 }
